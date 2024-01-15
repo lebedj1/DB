e110ed351363634a70713fcad3ce3471fce13b13
lebedj1 <47949255+lebedj1@users.noreply.github.com>
2024-01-15 10:46:04 +0100

    Add files via upload

---
+++ Mapping/Project.tsx
@@ -0,0 +1,5 @@
+  Description: { type: String, required: False ,   
+  Name: { type: string, required: False ,   
+  Start: { type: string, required: False ,   
+  ProjectID: { type: Number, required: False ,   
+  UserID: { type: Number, required: False ,   
